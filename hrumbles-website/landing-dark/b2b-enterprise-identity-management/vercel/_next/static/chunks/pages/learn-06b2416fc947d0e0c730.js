35b18a6b4bbcdf797162c92040897b074894777c
Your Correct GitHub Username <Kamesh.t@technoladders.com>
2025-10-10 10:37:13 +0530

    Update website Dockerfile for new directory structure

---
+++ hrumbles-website/landing-dark/b2b-enterprise-identity-management/vercel/_next/static/chunks/pages/learn-06b2416fc947d0e0c730.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[23128],{27393:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/learn",function(){return u(74925)}])}},function(n){n.O(0,[51657,58015,41664,79217,83889,14195,21692,92888,49774,40179],function(){return n(n.s=27393)}),_N_E=n.O()}]);
---
+++ hrumbles-website/landing-dark/b2b-enterprise-identity-management/vercel/_next/static/chunks/pages/secure-smarter-99f57e17e2a0d859c730.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[21850],{18681:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/secure-smarter",function(){return u(87499)}])}},function(n){n.O(0,[51657,58015,41664,79217,83889,14195,75359,92888,49774,40179],function(){return n(n.s=18681)}),_N_E=n.O()}]);
